f7c66570dcb25f687b905c59a4959d5871680517
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-04-10 13:48:23 -0300

    Alterar projeto para js

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+// import { useHistory } from 'react-router-dom'
+import './App.css'
+
+import cards from './assets/cards-home.png'
+
+function App() {
+  const [nick, setNick] = useState('')
+  // const history = useHistory()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log(event)
+    localStorage.setItem('nick', nick)
+    // history.push('/room')
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 p-2">
+          <form className="box text-center" onSubmit={handleSubmit}>
+            <h1 className="">Presidente</h1>
+            <input
+              className="input-default mb-4"
+              placeholder="Digite seu Nickname..."
+              onChange={e => setNick(e.target.value)}
+            />
+            <button className="btn-black mb-2" type="submit">
+              Entrar
+            </button>
+          </form>
+          <img className="img" src={cards} alt="Cards" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
