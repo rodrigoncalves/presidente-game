4d5e50a1f9864bc159c021306fcc84fdd313e030
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-04-10 14:46:20 -0300

    Add form validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import './Login.css'
 
 import cards from '../assets/cards-home.png'
@@ -8,26 +9,32 @@ export default function Login() {
   const savedNick = localStorage.getItem('nick')
   const [nick, setNick] = useState(savedNick)
   const history = useHistory()
+  const { handleSubmit, register, errors } = useForm()
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    if (nick) {
-      localStorage.setItem('nick', nick)
-    }
-    history.push('/room')
+  const login = ({ nick }) => {
+    localStorage.setItem('nick', nick)
+    history.push('/room', { nick })
   }
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 p-2">
-          <form className="box text-center" onSubmit={handleSubmit}>
+          <form className="box text-center" onSubmit={handleSubmit(login)}>
             <h1 className="">Presidente</h1>
+            <p className="error-message">{errors.nick && errors.nick.message}</p>
             <input
+              name="nick"
               className="input-default mb-4"
               placeholder="Digite seu Nickname..."
               value={nick}
-              onChange={e => setNick(e.target.value)}
+              onChange={e => {
+                const value = e.target.value
+                if (value.length <= 20) {
+                  setNick(value)
+                }
+              }}
+              ref={register({ required: 'Campo obrigatório' })}
             />
             <button className="btn-black mb-2" type="submit">
               Entrar
